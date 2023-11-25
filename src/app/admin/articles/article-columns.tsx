fce635c88817cf4199b6c085ad393e72dfec79ae
raphauy <rapha.uy@rapha.uy>
2023-11-25 20:18:34 -0300

    Artículos

--- src/app/admin/articles/article-columns.tsx
+++ src/app/admin/articles/article-columns.tsx
@@ -21,9 +21,9 @@ export const columns: ColumnDef<ArticleDAO>[] = [
 
 
       return (
-        <Link href={`/admin/articles/${data.id}`}>
-          <Image src={data.image} width={150} height={150} className="w-80 rounded-md" alt={data.title} />
-        </Link>
+          <Link href={`/admin/articles/${data.id}`}>
+            <Image src={data.image} width={150} height={150} className="min-w-[150px] rounded-md" alt={data.title} />
+          </Link>      
       )
     },
   },
@@ -45,9 +45,21 @@ export const columns: ColumnDef<ArticleDAO>[] = [
       const data = row.original;
 
       return (
-        <Link href={`/admin/articles/${data.id}`} className="">
-          <Button variant="ghost" className="px-0">{data.title}</Button>
-        </Link>
+        <div className="flex flex-col justify-between h-fit gap-12 text-center">
+          <Link href={`/admin/articles/${data.id}`} className="">
+            <Button variant="ghost" className="px-0">{data.title}</Button>
+          </Link>
+          <p>{data.authorName}</p>
+          <div className="flex justify-center">
+              {
+                  data.categories?.map((category) => (
+                      <div key={category} className="bg-naranja text-verde-oscuro w-fit px-4 rounded-full font-bold">
+                          {category}
+                      </div>
+                  ))
+              }
+          </div>
+        </div>
     );
     },
   },
@@ -70,10 +82,8 @@ export const columns: ColumnDef<ArticleDAO>[] = [
       const data = row.original;
 
       return (
-        <div className="flex flex-col justify-between gap-10">
+        <div className="">
           <p>{data.summary}</p>
-
-          <p className="self-center">{data.status}</p>
       </div>
     );
     },
@@ -111,52 +121,6 @@ export const columns: ColumnDef<ArticleDAO>[] = [
     },
   },
 
-  {
-    accessorKey: "categories",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          className="pl-0 dark:text-white"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Categories
-          <ArrowUpDown className="w-4 h-4 ml-1" />
-        </Button>
-      );
-    },
-    cell: ({ row }) => {
-      const data = row.original;
-
-      return (
-        <div>
-          {
-              data.categories?.map((category) => (
-                  <div key={category} className="bg-naranja text-verde-oscuro w-fit px-4 rounded-full font-bold">
-                      {category}
-                  </div>
-              ))
-          }
-        </div>
-);
-    }
-  },
-
-  {
-    accessorKey: "authorName",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          className="pl-0 dark:text-white"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Author
-          <ArrowUpDown className="w-4 h-4 ml-1" />
-        </Button>
-      );
-    },
-  },
 
   // {
   //   accessorKey: "role",
@@ -182,12 +146,15 @@ export const columns: ColumnDef<ArticleDAO>[] = [
 
       const publish= data.status !== "published" ? true : false
       return (
-        <div className="flex items-center justify-end gap-2">
+        <div className="space-y-10">
           <PublishUnpublishDialog id={data.id} publish={publish} title={data.title} />
-          <Link href={`/admin/articles/${data.id}`}>
-            <Button variant="ghost"><Eye  /></Button>
-          </Link>
-          <DeleteArticleDialog description={description} id={data.id} />
+
+          <div className="flex items-center">
+            <Link href={`/admin/articles/${data.id}`}>
+              <Button variant="ghost"><Eye  /></Button>
+            </Link>
+            <DeleteArticleDialog description={description} id={data.id} />
+          </div>
         </div>
       );
     },
