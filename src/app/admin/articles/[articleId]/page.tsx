f6bffa457fa78b4fb4429fa9555c66c879e6e05d
raphauy <rapha.uy@rapha.uy>
2023-11-30 08:26:23 -0300

    Small changes

--- src/app/admin/articles/[articleId]/page.tsx
+++ src/app/admin/articles/[articleId]/page.tsx
@@ -1,10 +1,8 @@
+import ReportCard from "@/app/[locale]/reports/report-card";
 import { getArticleDAO } from "@/services/article-services";
 import NovelOnClient from "./editor-on-client";
 import FilesSection from "./files";
-import ImageSection from "./image";
 import SummarySection from "./summary";
-import { format } from "date-fns";
-import ArticleCard from "./article-card";
 
 type Props = {
     params: {
@@ -20,7 +18,7 @@ export default async function Novel({ params }: Props) {
 
     return (
         <main className="flex flex-col p-1 md:p-4 xl:p-8 space-y-2">
-            <ArticleCard article={article} admin />
+            <ReportCard article={article} admin />
 
             <NovelOnClient articleId={articleId} initialContent={article.content} />
 
